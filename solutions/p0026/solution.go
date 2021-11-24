4785f31b1269335aa57046189fcd16a14393e531
Brisk <feyfree@163.com>
2021-11-24 23:43:50 +0800

    [REV] add solution

--- solutions/p0026/solution.go
+++ solutions/p0026/solution.go
@@ -1,5 +1,17 @@
 package p0026
 
 func removeDuplicates(nums []int) int {
-	return 0
+	n := len(nums)
+	if n <= 1 {
+		return n
+	} else {
+		i := 0
+		for j := 1; j < n; j++ {
+			if nums[j] != nums[i] {
+				i++
+				nums[i] = nums[j]
+			}
+		}
+		return i + 1
+	}
 }
