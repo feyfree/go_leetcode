e3d60c96dd9576fb08d5bc7590a84510f215c89c
leilei <feyfree@163.com>
2022-01-20 17:56:34 +0800

    [REV] add new leetcode solution

--- solutions/p0084/solution.go
+++ solutions/p0084/solution.go
@@ -1,5 +1,35 @@
 package p0084
 
+import "container/list"
+
 func largestRectangleArea(heights []int) int {
-	return 0
+	n := len(heights)
+	stack := list.New()
+	result := 0
+	i := 0
+	for i <= n {
+		var height int
+		if i == n {
+			height = 0
+		} else {
+			height = heights[i]
+		}
+		if stack.Len() == 0 || height >= heights[stack.Back().Value.(int)] {
+			stack.PushBack(i)
+			i++
+		} else {
+			h := heights[stack.Back().Value.(int)]
+			stack.Remove(stack.Back())
+			var w int
+			if stack.Len() > 0 {
+				w = i - stack.Back().Value.(int) - 1
+			} else {
+				w = i
+			}
+			if h*w > result {
+				result = h * w
+			}
+		}
+	}
+	return result
 }
