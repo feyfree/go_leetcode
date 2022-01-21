0a8433144831be683e0aa27842c8cf120f3a944b
leilei <feyfree@163.com>
2022-01-21 11:42:00 +0800

    [REV] modify solution comment

--- solutions/p0084/solution.go
+++ solutions/p0084/solution.go
@@ -6,25 +6,26 @@ func largestRectangleArea(heights []int) int {
 	n := len(heights)
 	stack := list.New()
 	result := 0
-	i := 0
-	for i <= n {
+	end := 0
+	for end <= n {
 		var height int
-		if i == n {
+		if end == n {
 			height = 0
 		} else {
-			height = heights[i]
+			height = heights[end]
 		}
 		if stack.Len() == 0 || height >= heights[stack.Back().Value.(int)] {
-			stack.PushBack(i)
-			i++
+			stack.PushBack(end)
+			end++
 		} else {
+			// 这个过程中 end 不会 ++  所以在这个过程中实际上是 end 确定, start 不断变化的过程
 			h := heights[stack.Back().Value.(int)]
 			stack.Remove(stack.Back())
 			var w int
 			if stack.Len() > 0 {
-				w = i - stack.Back().Value.(int) - 1
+				w = end - stack.Back().Value.(int) - 1
 			} else {
-				w = i
+				w = end
 			}
 			if h*w > result {
 				result = h * w
---
+++ solutions/p0084/solution_test.go
@@ -0,0 +1,8 @@
+package p0084
+
+import "testing"
+
+func TestSolution(t *testing.T) {
+	heights := [...]int{1, 2, 3}
+	largestRectangleArea(heights[:])
+}
