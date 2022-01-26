88732fed323dcd6b4efad5bd13b806cb6ea7b388
leilei <feyfree@163.com>
2022-01-26 17:02:05 +0800

    [REV] add new leetcode solution

--- solutions/p0098/solution.go
+++ solutions/p0098/solution.go
@@ -1,9 +1,29 @@
 package p0098
 
-import "github.com/feyfree/go_leetcode/structures"
+import (
+	"github.com/feyfree/go_leetcode/structures"
+	"math"
+)
 
 type TreeNode = structures.TreeNode
 
+var prev int
+
 func isValidBST(root *TreeNode) bool {
-	return false
+	prev = math.MinInt64
+	return inOrder(root)
+}
+
+func inOrder(root *TreeNode) bool {
+	if root == nil {
+		return true
+	}
+	if !inOrder(root.Left) {
+		return false
+	}
+	if root.Val <= prev {
+		return false
+	}
+	prev = root.Val
+	return inOrder(root.Right)
 }
