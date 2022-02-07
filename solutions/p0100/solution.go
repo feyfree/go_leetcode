9dd610ef0cbbca57a6707dec8ea02f891604d0c4
leilei <feyfree@163.com>
2022-02-07 18:05:46 +0800

    [REV] add new leetcode solution

--- solutions/p0100/solution.go
+++ solutions/p0100/solution.go
@@ -1,1 +1,13 @@
 package p0100
+
+import "github.com/feyfree/go_leetcode/structures"
+
+type TreeNode = structures.TreeNode
+
+func isSameTree(p *TreeNode, q *TreeNode) bool {
+	same := (p == nil && q == nil) || (p != nil && q != nil && p.Val == q.Val)
+	if same && p != nil {
+		same = isSameTree(p.Left, q.Left) && isSameTree(p.Right, q.Right)
+	}
+	return same
+}
