ba4630ba3d0bf6bc131e3dd807b1c1cf353b3f42
leilei <feyfree@163.com>
2022-02-21 18:46:37 +0800

    [REV] add new solution

--- solutions/p0114/solution.go
+++ solutions/p0114/solution.go
@@ -1,9 +1,22 @@
 package p0114
 
-import "github.com/feyfree/go_leetcode/structures"
+import (
+	"github.com/feyfree/go_leetcode/structures"
+)
 
 type TreeNode = structures.TreeNode
 
 func flatten(root *TreeNode) {
-
+	if root == nil {
+		return
+	}
+	flatten(root.Left)
+	right := root.Right
+	root.Right = root.Left
+	root.Left = nil
+	for root.Right != nil {
+		root = root.Right
+	}
+	flatten(right)
+	root.Right = right
 }
